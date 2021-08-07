a82bf56c5cdd6512e7a8547dc733176d7a051966
Alik1604 <rudenko.albert.n@gmail.com>
2021-08-07 16:47:25 +0300

    add icons

--- main.js
+++ main.js
@@ -6,19 +6,19 @@ let battleScore1 = 0;
 let battleScore2 = 0;
 let PlayerChoise = "Rock"; 
 let Pattern = false;
-while (Pattern == false) {
-PlayerChoise = prompt("Enter your choise",);
-ComputerChoise = Math.floor(Math.random() * (max - min) ) + min;
-if (ComputerChoise === 1){
-    ComputerChoise2 = "Rock";
- }else if ( ComputerChoise === 2){
-     ComputerChoise2 = "Paper";
- }else if ( ComputerChoise === 3){
-     ComputerChoise2 = "Scissors";
- }
+//while (Pattern == false) {
+//PlayerChoise = prompt("Enter your choise",);
+//ComputerChoise = Math.floor(Math.random() * (max - min) ) + min;
+//if (ComputerChoise === 1){
+//    ComputerChoise2 = "Rock";
+// }else if ( ComputerChoise === 2){
+//     ComputerChoise2 = "Paper";
+// }else if ( ComputerChoise === 3){
+//     ComputerChoise2 = "Scissors";
+// }
 console.log("CPU",ComputerChoise2);
 console.log("You",PlayerChoise);
-if (PlayerChoise == ComputerChoise2){
+//if (PlayerChoise == ComputerChoise2){
 battleScore1 += 0;
 battleScore2 += 0;
 }else if(PlayerChoise === "Rock" && ComputerChoise2 === "Scissors"){
@@ -37,8 +37,8 @@ battleScore2 += 1;
 if(battleScore1 == 5 || battleScore2 == 5){
 Pattern = true;
 }
-console.log("CPU", battleScore2)
-console.log("You", battleScore1)
+//console.log("CPU", battleScore2)
+//console.log("You", battleScore1)
 } 
 
 
